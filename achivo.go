dcdfddc5447bdb8ab131daf72bc32c2fcd2f151b
Jose Wannan <2993999310101@ingenieria.usac.edu.gt>
2021-11-14 02:27:55 -0600

    cas

--- achivo.go
+++ achivo.go
@@ -6,6 +6,7 @@ func JuegoMongo1(w http.ResponseWriter, r http.Request) {
     if err != nil {
         fmt.Println(err)
     }
+    body["way"] = "RabbitMQ"
     data, err := json.Marshal(body)
 
 	//observar la info
@@ -52,4 +53,81 @@ func JuegoMongo1(w http.ResponseWriter, r http.Request) {
     json.NewEncoder(w).Encode(result)
 
     //fmt.Fprintf(w, "Se Funcion")
+}
+
+
+func (s *server) RegGame(ctx context.Context, in gamepb.GameRequest) (gamepb.GameResponse, error) {
+
+    var winner int
+    result := ""
+
+    id := in.GetGame().GetId()
+    juego := in.GetGame().GetJuego()
+    max := in.GetGame().GetMax()
+
+    desicion := int(id)
+    maximo := int(max)
+
+    if desicion == 1 {
+        winner = juego1(maximo)
+    } else if desicion == 2 {
+        winner = juego2(maximo)
+    } else if desicion == 3 {
+        winner = juego3(maximo)
+    } else {
+        winner = 0
+    }
+
+    //    identificador := strconv.FormatInt(id, 10)
+    //ganador := strconv.Itoa(winner)
+
+    byt := []byte(`{"ID":0}`)
+
+    var dat map[string]interface{}
+    err := json.Unmarshal(byt, &dat); 
+
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println(dat)
+
+    dat["ID"] = desicion
+    dat["juego"] = juego
+    dat["max"] = winner
+    dat["players"] = maximo
+    dat["worker"] = "kafka"
+
+    data, err := json.Marshal(dat)
+
+    peticion, _ := json.Marshal(gameStruct{
+        Id:      desicion,
+        juego:   juego,
+        max: winner,
+        players: maximo,
+        worker:  "kafka",
+
+    })
+
+    petition := bytes.NewBuffer(data)
+
+    response, err := http.Post("http://34.122.191.135:2062/", "application/json", petition)
+    if err != nil {
+        log.Fatalln("Error sending info", err)
+    }
+    //response.Body.Close()
+
+    body, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        log.Fatalln(err)
+    }
+    // SE NECESITA OTRO ENDPOINT
+    result += string(body)
+    fmt.Println(result + "------------")
+    //result := " ID: " + identificador + " Juego: " + juego + " Ganador!: " + ganador
+
+    res := &gamepb.GameResponse{
+        Result: result,
+    }
+
+    return res, nil
 }
